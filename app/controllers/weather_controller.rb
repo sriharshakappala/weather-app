53b9de80049ff331ad6a2e861dc84a8ba554fadc
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2018-06-26 23:02:09 +0530

    Remove redundant API layer

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,21 +4,6 @@ class WeatherController < ApplicationController
     @forecast = WeatherFetchService.new(params[:city]).forecast
   end
 
-  def city_forecast
-    @city = City.find_by_code params[:code]
-    key = "forecast_api_#{params[:code]}"
-    cached_response = $redis.get(key)
-    if cached_response.present?
-      @forecast = JSON.parse cached_response
-    else
-      forecast = Weather.for_city(params[:code]).forecast(10).to_json
-      $redis.set(key, forecast)
-      $redis.expire(key, 3600)
-      @forecast = JSON.parse forecast
-    end
-    @condition = @forecast.first
-  end
-
   def city_forecast_api
     @forecast = WeatherApiService.new(params[:code]).forecast
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get '/weather/forecast'     => 'weather#forecast'
-  get '/city/forecast/:code'  => 'weather#city_forecast', defaults: { format: 'json' }
-  get 'new_home'              => 'home#new_index'
 
   resource :api do
     resource :v1 do
