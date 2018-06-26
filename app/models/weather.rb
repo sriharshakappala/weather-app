8f653c055f2178d1ca99770c5f6600ecc5fe65c2
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2018-06-26 15:52:39 +0530

    Add scopes for city and forecast in Weather model

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,4 +3,7 @@ class Weather
   include Mongoid::Timestamps
   include Mongoid::Attributes::Dynamic
   store_in collection: "weather"
+
+  scope :for_city, -> (code) { where(code: code) }
+  scope :forecast, -> (days) { where(date: (Date.today..(Date.today + days.days))) }
 end
