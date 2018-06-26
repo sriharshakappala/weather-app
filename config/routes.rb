df377983dd6d1c928666666562f62d5d1479131f
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2018-06-26 23:46:21 +0530

    Add Jquery UI asset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get '/weather/forecast'     => 'weather#forecast'
+  get '/climate'              => 'climate#index'
+  post '/get_suggestions'     => 'cities#get_suggestions'
 
   resource :api do
     resource :v1 do
