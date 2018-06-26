4e040e269548284c4940e89e49551f1998dd9fd8
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2018-06-26 20:30:02 +0530

    Add endpoint for city forecast

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,4 +4,10 @@ class WeatherController < ApplicationController
     @forecast = WeatherFetchService.new(params[:city]).forecast
   end
 
+  def city_forecast
+    @city = City.find_by_code params[:code]
+    @forecast = Weather.for_city(params[:code]).forecast(10)
+    @condition = @forecast.first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get '/weather/forecast' => 'weather#forecast'
+  get '/city/forecast/:code' => 'weather#city_forecast', defaults: { format: 'json' }
 end
