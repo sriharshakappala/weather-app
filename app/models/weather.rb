2ae8327e36f59bc93f964ce264f2c4e280be89b9
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2018-06-26 20:32:51 +0530

    Add current condition in JBuilder file and add order by date in model scope

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -5,5 +5,5 @@ class Weather
   store_in collection: "weather"
 
   scope :for_city, -> (code) { where(code: code) }
-  scope :forecast, -> (days) { where(date: (Date.today..(Date.today + days.days))) }
+  scope :forecast, -> (days) { where(date: (Date.today..(Date.today + days.days))).order("date ASC") }
 end
